3b7de828a30c6d36b7f882a8953b24c4c6b16681
Steven Verwerft <33197418+StevenVerwerft@users.noreply.github.com>
2018-05-24 16:23:40 +0200

    wrote documentation for the greedy search, problem: solution is corrup => cities multiple occurrences in route

--- functions.py
+++ functions.py
@@ -1,8 +1,9 @@
 import numpy as np
+import random
 
 
 def euclidean_distance(point1, point2):
-    return np.sqrt(sum([(point1[0] - point2[0])**2, (point1[1] - point2[1])**2]))
+    return np.sqrt(sum([(point1[0] - point2[0]) ** 2, (point1[1] - point2[1]) ** 2]))
 
 
 def distance_matrix(city_array):
@@ -17,9 +18,9 @@ def distance_matrix(city_array):
     matrix = np.zeros(shape=(N, N))
 
     # append the matrix with right-triangular elements (assumption distance matrix is symmetrical)
-    for i in range(N-1):
+    for i in range(N - 1):
         matrix[i, i] = np.inf  # distance on diagonal (between same cities) to infinite
-        for j in range(i+1, N):
+        for j in range(i + 1, N):
             matrix[i, j] = euclidean_distance(city_array[i], city_array[j])
 
     return matrix
@@ -29,7 +30,7 @@ def get_pairs(array):
     pairs = []
     for i in range(len(array)):
         try:
-            pairs.append(sorted([array[i], array[i+1]]))
+            pairs.append(sorted([array[i], array[i + 1]]))
         except IndexError:
             pass
     return pairs
@@ -44,20 +45,98 @@ def get_distances(pairs, distance_matrix):
 
 
 def total_distance(solution, distance_matrix):
-
     return sum(get_distances(get_pairs(solution), distance_matrix))
 
 
 def two_opt(route, move):
-
     assert move[0] < move[1], 'move pair not increasing: ({}, {})'.format(move[0], move[1])
-    newRoute = route[:move[0]] + list(reversed(route[move[0]: move[1]+1])) + route[move[1]+1:]
+    newRoute = route[:move[0]] + list(reversed(route[move[0]: move[1] + 1])) + route[move[1] + 1:]
     return newRoute
 
 
 def swap(route, move):
-
     assert move[0] < move[1], 'move pair not increasing'
-    newRoute = route[:move[0]] + route[move[1]: move[1]+1] + route[move[0]+1: move[1]] + route[move[0]: move[0]+1] \
-               + route[move[1]+1:]
+    newRoute = route[:move[0]] + route[move[1]: move[1] + 1] + route[move[0] + 1: move[1]] + route[move[0]: move[0] + 1] \
+               + route[move[1] + 1:]
     return newRoute
+
+
+def greedy_search(coordinates, distance_matrix):
+    """
+    :param coordinates: Array containing the coordinates of the cities
+    :param distance_matrix: Matrix holding the Euclidean distances between each of the cities
+    :return:
+    """
+    # initialization
+    n_cities = len(coordinates)
+
+    # original distance matrix should remain unaffected after greedy search
+    distance_matrix = distance_matrix.copy()
+    # make a greedy solution
+    route_indices = []
+    route_coordinates = []
+
+    # 1 pick random first city
+    starting = random.choice(range(n_cities))
+
+    route_indices.append(starting)
+
+    # The city coordinates are adjusted simultanously with the route, alternatively this can be done in the end
+    # CHECK IF THIS RESULTS INTO THE SAME SOLUTION8
+    route_coordinates.append(coordinates[starting])
+
+    while len(route_indices) < len(coordinates):
+        # take the last occuring city in the route
+        previous_city = route_indices[-1]
+
+        # select all pairs between last city and other cities in distance matrix
+        # assumption: distances are symmetrical, only L/U-triangle needed
+        # distances are for one part found in a column and for the other part in a row, indexed by the previous city
+
+        neighbours = np.append(distance_matrix[:previous_city, previous_city], distance_matrix[previous_city,
+                                                                               previous_city:])
+
+        # the nearest city to the last city in the route is chosen as the next city
+        next_city = np.argmin(neighbours)  # position of nearest neighbour
+
+        # update the current route indices and the coordinates
+        route_indices.append(next_city)
+        route_coordinates.append(coordinates[next_city])
+
+        # CONSTRAINT: Each city allowed to be included in the route only once!
+        # After choosing a city based on the nearest distance in the distance matrix we can do two things:
+        # 1: Delete all occurrences of this city in the distance matrix (matrix becomes smaller in the end)
+        # => faster evaluation of minimal distances => more efficient
+        # 2: Set all the distances to and from this city on infinite
+        # => matrix stays the same size, but the indices in the matrix keep referencing to the cities in the array
+
+        # REMARK: assess the efficiency gains of the first method!
+
+        # Method 2: infinite distance to visited cities.
+        distance_matrix[:, previous_city], distance_matrix[previous_city, :] = np.inf, np.inf
+
+    return route_indices, route_coordinates
+
+
+def local_search(route, distance_matrix, coordinates, move_type='2-opt'):
+    max_iter = 100000
+    oldRoute = route.copy()
+    print(oldRoute)
+    coordinates = coordinates.copy()
+    for i in range(max_iter):
+        [a, b] = sorted(random.sample(range(len(route)), 2))
+
+        # 2-opt: reverse order of route between move points
+        if move_type == '2-opt':
+            newRoute = two_opt(oldRoute, [a, b])
+        # swap: swap cities on move points
+        if move_type == 'swap':
+            newRoute = route[:a] + route[b:b + 1] + route[a + 1:b] + route[a:a + 1] + route[b:]
+
+        if total_distance(newRoute, distance_matrix) - total_distance(oldRoute, distance_matrix) < 0:
+            print('better route found!')
+            print('total distance: ', total_distance(newRoute, distance_matrix))
+            oldRoute = newRoute
+            coordinates = two_opt(coordinates, [a, b])
+
+    return oldRoute, coordinates
--- main.py
+++ main.py
@@ -18,64 +18,13 @@ def main():
     # generate distance matrix
     d_matrix = distance_matrix(cities)  # fix modification by greedy search
     distances = copy.deepcopy(d_matrix)
-
-
-    # get nearest city
-    def greedy_search(cities, distance_matrix):
-
-        # make a greedy solution
-        solution = []
-        solution_coordinates = []
-        # 1 pick random first city
-        starting = random.choice(range(n_cities))
-        solution.append(starting)
-        # distance_matrix[:, starting], distance_matrix[starting, :] = np.inf, np.inf
-        solution_coordinates.append(cities[starting])
-
-        while len(solution) < len(cities):
-
-            previous = solution[-1]
-            neighbours = np.append(d_matrix[:previous, previous], d_matrix[previous, previous:])
-            next = np.argmin(neighbours)  # position of nearest neighbour
-            print('next: ', next)
-            solution.append(next)
-            solution_coordinates.append(cities[next])
-            distance_matrix[:, previous], distance_matrix[previous, :] = np.inf, np.inf
-
-        return solution, solution_coordinates
-
-    def local_search(route, distance_matrix, coordinates, move_type='2-opt'):
-
-        max_iter = 100000
-        oldRoute = route.copy()
-        print(oldRoute)
-        coordinates = coordinates.copy()
-        for i in range(max_iter):
-
-
-            [a, b] = sorted(random.sample(range(len(route)), 2))
-
-            # 2-opt: reverse order of route between move points
-            if move_type == '2-opt':
-                newRoute = two_opt(oldRoute, [a, b])
-            # swap: swap cities on move points
-            if move_type == 'swap':
-                newRoute = route[:a] + route[b:b+1] + route[a+1:b] + route[a:a+1] + route[b:]
-
-            if total_distance(newRoute, distance_matrix) - total_distance(oldRoute, distance_matrix) < 0:
-                print('better route found!')
-                print('total distance: ', total_distance(newRoute, distance_matrix))
-                oldRoute = newRoute
-                coordinates = two_opt(coordinates, [a, b])
-
-        return oldRoute, coordinates
-
-
+    print(d_matrix)
     # GREEDY SEARCH
     solution, solution_coordinates = greedy_search(cities, d_matrix)
     solution_coordinates.append(solution_coordinates[0])
-    del d_matrix  # delete garbage
     print('total distance = ', total_distance(solution, distances))
+    print(sorted(solution))
+    quit()
     plt.figure(1)
 
     plt.subplot(121)
