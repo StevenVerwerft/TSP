abb2cf5479b7cf8e8f96794be27527815a6bb9b8
Steven Verwerft <33197418+StevenVerwerft@users.noreply.github.com>
2018-05-28 19:17:28 +0200

    wip making geodistances work

--- functions.py
+++ functions.py
@@ -21,21 +21,73 @@ def readfile(filename):
     # Read node list
     nodelist = []
     N = int(Dimension)
+    min_x, min_y = 0.0, 0.0
+
     for i in range(0, N):
         x, y = infile.readline().strip().split()[1:]
         nodelist.append((float(x), float(y)))
 
+        if not EdgeWeightType == 'GEO':
+            if float(x) < min_x:
+                min_x = float(x)
+            if float(y) < min_y:
+                min_y = float(y)
+    nodelist = np.array(nodelist)
+
+    if not EdgeWeightType == 'GEO':
+
+        nodelist[:, 0] = nodelist[:, 0] - min_x
+        nodelist[:, 1] = nodelist[:, 1] - min_y
+
     # Close input file
+
     infile.close()
 
-    return nodelist
+    return nodelist.tolist(), EdgeWeightType
 
 
 def euclidean_distance(point1, point2):
     return np.sqrt(sum([(point1[0] - point2[0]) ** 2, (point1[1] - point2[1]) ** 2]))
 
 
-def distance_matrix(city_array):
+def distance_from_geo(point1, point2, measure='km'):
+
+    lon1, lon2 = to_degrees(point1[1]), to_degrees(point2[1])
+    lat1, lat2 = to_degrees(point1[0]), to_degrees(point2[0])
+
+    if measure == 'km':
+        radius = 6378.388
+    elif measure == 'miles':
+        radius = 3959
+
+    dtr = np.pi / 180.
+    phi_1 = lat1 * dtr
+    phi_2 = lat2 * dtr
+    lambda_1 = lon1 * dtr
+    lambda_2 = lon2 * dtr
+
+    d_phi = phi_2 - phi_1
+    d_lambda = lambda_2 - lambda_1
+
+    a = haversine(d_phi + np.cos(phi_1)*np.cos(phi_2)*haversine(d_lambda))
+    d = 2 * radius * np.arcsin(np.sqrt(a))
+
+    return d
+
+
+def to_degrees(point):
+
+    deg = int(point + .5)
+    min = point - deg
+    new_point = deg + (5./3. * min)
+    return new_point
+
+
+def haversine(angle):
+    return np.sin(angle/2.)**2
+
+
+def distance_matrix(city_array, distance_type=None, measure='km'):
     """
 
     :param city_array: Array containing coordinates for the different cities
@@ -47,12 +99,23 @@ def distance_matrix(city_array):
     matrix = np.zeros(shape=(N, N))
 
     # append the matrix with right-triangular elements (assumption distance matrix is symmetrical)
-    for i in range(N - 1):
-        matrix[i, i] = np.inf  # distance on diagonal (between same cities) to infinite
-        for j in range(i + 1, N):
-            matrix[i, j] = round(euclidean_distance(city_array[i], city_array[j]))
 
-    matrix[N-1, N-1] = np.inf
+    if not distance_type == 'GEO':
+        for i in range(N - 1):
+            matrix[i, i] = np.inf  # distance on diagonal (between same cities) to infinite
+            for j in range(i + 1, N):
+                matrix[i, j] = round(euclidean_distance(city_array[i], city_array[j]))
+
+        matrix[N-1, N-1] = np.inf
+
+    if distance_type == 'GEO':
+        for i in range(N - 1):
+            matrix[i, i] = np.inf
+            for j in range(i +1, N):
+                matrix[i, j] = round(distance_from_geo(city_array[i], city_array[j], measure=measure))
+
+        matrix[N-1, N-1] = np.inf
+
     return matrix
 
 
@@ -148,17 +211,28 @@ def greedy_search(coordinates, distance_matrix):
     return route_indices, route_coordinates
 
 
-def local_search(route, distance_matrix, coordinates, move_type='2-opt', max_iter=300000, max_time=None):
+def local_search(route, distance_matrix, coordinates, move_type='2-opt', max_iter=300000, max_time=None,
+                 first_x=None):
 
-    global goalfunctions_local_search
-    goalfunctions_local_search = []
+    # auxiliary variables
+    n_cities = len(route)  # the amount of different cities in the TSP
+    oldRouteDistance = total_distance(route, distance_matrix)
+    BestGoalfunctionValues = [oldRouteDistance]
+    AllGoalfunctionValues = [oldRouteDistance]
 
+
+    # memories
+    if first_x:
+        firstXMemory = []
+
+    # stopping criterion = time
     if max_time:
         starttime = time.time()
         max_iter = np.inf
         time_interval = range(1, int(max_time)+1)
         print(time_interval)
 
+    # to be checked if these variables stay 'local' or if deepcopy is necessary
     oldRoute = route.copy()
     coordinates = coordinates.copy()
     i = 1
@@ -168,25 +242,51 @@ def local_search(route, distance_matrix, coordinates, move_type='2-opt', max_ite
             if (time.time() - starttime) > max_time:
                 break
 
-        [a, b] = sorted(random.sample(range(len(route)), 2))
+        # pick two random cities in the existing route
+        # each position in the route list refers to a unique city
+        move = sorted(random.sample(range(n_cities), 2))
 
         # 2-opt: reverse order of route between move points
         if move_type == '2-opt':
-            newRoute = two_opt(oldRoute, [a, b])
+            # make a temporary new route by two-opting the two randomly chosen cities
+            newRoute = two_opt(oldRoute, move)
+
         # swap: swap cities on move points
-        if move_type == 'swap':
+        elif move_type == 'swap':
+            # make a temporary new route by swapping the two randomly chosen cities
+            a, b = move[0], move[1]
             newRoute = route[:a] + route[b:b + 1] + route[a + 1:b] + route[a:a + 1] + route[b:]
 
-        if total_distance(newRoute, distance_matrix) - total_distance(oldRoute, distance_matrix) < 0:
-            # print('better route found!')
-            # print('total distance: ', total_distance(newRoute, distance_matrix))
-            oldRoute = newRoute
-            coordinates = two_opt(coordinates, [a, b])
+        # check if the goalfunction of the new route is smaller than the goalfunction of the oldroute
+        newRouteDistance = total_distance(newRoute, distance_matrix)
+        AllGoalfunctionValues.append(newRouteDistance)  # memory for all visitied solutions
+
+        if newRouteDistance - oldRouteDistance < 0:
+            if not first_x:
+                # local search performs best improvement strategy
+                oldRoute = newRoute
+                coordinates = two_opt(coordinates, move)
+                oldRouteDistance = newRouteDistance
+                BestGoalfunctionValues.append(oldRouteDistance)
+
+            if first_x:
+                # local search performs first x improvement strategy
+                firstXMemory.append((move, newRouteDistance))
+                if len(firstXMemory) == first_x:
+                    # pick the best solution from the memory
+                    # sort the moves in the first x memory by ascending goalfunction value
+                    firstXMemory = sorted(firstXMemory, key=lambda x: x[1])
+                    best_move = firstXMemory[0][0]
+                    oldRoute = two_opt(oldRoute, best_move)
+                    oldRouteDistance = firstXMemory[0][1]
+                    BestGoalfunctionValues.append(oldRouteDistance)
 
     if max_time:
         print('calculation time (seconds): {}s'.format(round(time.time() - starttime), 4))
-        print('total iterations (thousands): ', round(i / 1000))
-    return oldRoute, coordinates
+        print('total iterations: {:.2E}'.format(i))
+
+    print('final goalfunction value: ', BestGoalfunctionValues[-1])
+    return oldRoute, coordinates, BestGoalfunctionValues, AllGoalfunctionValues
 
 
 def plot_coordinates(coordinate_array):
